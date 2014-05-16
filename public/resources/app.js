3deb25b6c7d7ad69fad1a0fb394e6c8827b532ce
mikeymike <mike@webthatworks.com>
2014-05-16 14:21:05 +0100

    Initial Commit

--- public/resources/app.js
+++ public/resources/app.js
@@ -3,15 +3,45 @@
  */
 // Get host without port and create new WebSocket
 var host = window.document.location.host.replace(/:.*/, ''),
-    ws = new WebSocket('ws://' + host + ':8080');
+    ws = null,
+    socketHost = 'ws://' + host + ':8080',
+    tweetCount = 0,
+    chartData = [
+        ['Tweet Count', 'Waiting for term...'],
+        [0, 0]
+    ],
+    dashboard = null, control = null, chart = null;
 
+connectSocket();
+
+/**
+ * Connect to Server using WebSocket
+ */
+function connectSocket(){
+    ws = new WebSocket(socketHost);
+}
+
+/**
+ * WebSocket Open function
+ */
+ws.onopen = function(){
+    console.log("Connection Open... awaiting term");
+};
+
+ws.onclose = function(){
+    console.log("Connection Closed... retying...");
+
+    while(ws.readyState === WebSocket.CLOSED || ws.readyState === WebSocket.CLOSING){
+        connectSocket();
+    }
+};
 
 /**
  * WebSocket Error function, log errors
  * @param err
  */
 ws.onerror = function(err){
-    console.log(err);
+    console.log("Errrr we got a problem... ", err);
 };
 
 /**
@@ -19,7 +49,25 @@ ws.onerror = function(err){
  * @param e
  */
 ws.onmessage = function(e){
-    console.log(JSON.parse(e.data));
+    tweetCount++;
+    var tweetData = JSON.parse(e.data);
+
+    if(tweetCount === 1){
+        chartData.splice(1,1);
+    }
+
+        control.setState({
+            'range': {
+                'start': Math.max(0, chartData.length-50)
+            }
+        });
+
+    chartData.push([
+        tweetCount,
+        tweetData.termCount /  tweetData.tweetLength
+    ]);
+
+    drawChart();
 };
 
 
@@ -27,14 +75,148 @@ ws.onmessage = function(e){
  * Subscribe via WebSocket to a tweet tracking
  */
 function trackTweets(){
-    ws.send(document.getElementById('search-term').value);
+    var term = document.getElementById('search-term').value;
+    ws.send(term);
+
+    chartData[0][1] = term.toLowerCase();
 }
 
 /**
  * Dom Ready Func
  */
 document.addEventListener("DOMContentLoaded", function(){
-    var trackBtn = document.getElementById('track-btn');
-    trackBtn.addEventListener('click', trackTweets, false);
+
+    // Get Form
+    var form = document.getElementById("tweet-form");
+
+    // Override submit
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        trackTweets();
+    });
+
+
+    document.getElementById("kill-twitter").addEventListener('click', function(){
+        console.log('Send Kill Signal');
+        ws.send('kill-twitter-connection')
+    });
+
+    document.getElementById("resume-twitter").addEventListener('click', function(){
+        console.log('Send Resume Signal');
+        ws.send('resume-twitter-connection')
+    });
+
 }, false);
 
+google.load('visualization', '1.1', {packages: ['corechart', 'controls']});
+google.setOnLoadCallback(createChart);
+
+function drawChart(){
+    control.draw();
+    dashboard.draw(google.visualization.arrayToDataTable(chartData));
+}
+
+
+function createChart() {
+    dashboard = new google.visualization.Dashboard(document.getElementById('dashboard'));
+
+    control = new google.visualization.ControlWrapper({
+        controlType: 'ChartRangeFilter',
+        containerId: 'control',
+        options: {
+            filterColumnIndex: 0,
+            ui: {
+                chartType: 'LineChart',
+                chartOptions: {
+                    chartArea: {
+                        width: '90%',
+                        height: '50px'
+                    },
+                    hAxis: {
+                        baselineColor: 'none',
+                        gridlines: {
+                            color: 'none',
+                            count: -1
+                        },
+                        textStyle: {
+                            color: 'transparent'
+                        }
+                    },
+                    vAxis: {
+                        baselineColor: 'none',
+                        gridlines: {
+                            color: 'none',
+                            count: -1
+                        },
+                        textStyle: {
+                            color: '#ecf0f1'
+                        }
+                    },
+                    colors: ["#16a085", "#ecf0f1"],
+                    backgroundColor: {
+                        fill: "#34495e" // Wet Asphalt
+                    }
+                },
+                minRangeSize: 1
+            }
+        }
+    });
+
+    chart = new google.visualization.ChartWrapper({
+        chartType: 'LineChart',
+        containerId: 'chart',
+        options: {
+            legend: {
+                position: 'bottom',
+                textStyle: {
+                    color: '#ecf0f1'
+                }
+            },
+            hAxis: {
+                title: 'Tweets',
+                titleTextStyle: {
+                    color: '#ecf0f1'
+                },
+                color: '#ecf0f1',
+                baselineColor: '#ecf0f1',
+                gridlines: {
+                    color: 'none',
+                    count: -1
+                },
+                textStyle: {
+                    color: '#ecf0f1'
+                }
+            },
+            vAxis: {
+                title: 'Term Frequency',
+                titleTextStyle: {
+                    color: '#ecf0f1'
+                },
+                color: '#ecf0f1',
+                baselineColor: '#ecf0f1',
+                gridlines: {
+                    color: 'none',
+                    count: -1
+                },
+                textStyle: {
+                    color: '#ecf0f1'
+                }
+            },
+//            animation: {
+//                duration: 100,
+//                easing: 'linear'
+//            },
+            chartArea: {
+                width: "90%"
+            },
+            backgroundColor: {
+                fill: "#34495e" // Wet Asphalt
+            },
+            pointSize: 8,
+            colors: ["#16a085", "#ecf0f1"]
+        }
+    });
+
+    dashboard.bind(control, chart);
+    dashboard.draw(google.visualization.arrayToDataTable(chartData));
+}
--- server.js
+++ server.js
@@ -1,105 +1,29 @@
 var http = require('http')
     port = process.env.port || 1337,
-    fs = require('fs')
-    url = require('url'),
+    azure = require('azure'),
+    tableService = azure.createTableService('portalvhdsxgbm1yx4fh6y5','tPfSNHixG2NFja2ZBdMZA+hLcYqsk1YP/kaIqsMqj3RsLjh8GD+MlZE9ZkUBvCfYSXiLbqU6sK6WhTt1XPBqCQ=='),
+    tableName = 'tweetTrackerApp',
     Router = require('node-simple-router'),
     WebSocketServer = require('ws').Server,
-    session = require('sesh').magicSession(),
     twitter = require('twitter'),
-    OAuth = require('oauth').OAuth,
-    domain = 'http://dwa-app1.azurewebsites.net';
+    twitterKeys = {
+        consumer: 'GnB0DXhhkonUwDeG9Ardkw',
+        consumer_secret: 'iwWsFZeg9ncdWl3KjDy1X9mnw1uioN6ZFUx8loK68',
+        access_token: '488320252-o4VOIiikbvtVF7HyFD7Zb4rLJPHRD7JAre52symJ',
+        access_token_secret: 'h95Hb2yqO0ciTIgmp7778MjojOPvVgHo7Ht6YD9nvWTAZ'
+    };
+
+// TODO: Retry on fail
+//tableService.deleteTable(tableName, function(){});
+
+
+tableService.createTableIfNotExists(tableName, function(err){});
 
 
 /**
  * HTTP Server
  */
 var router = new Router();
-
-// Cache HTML
-var index;
-fs.readFile('index.html', function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        index = data;
-    }
-});
-
-/**
- * App Route
- */
-router.get('/', function (req, res) {
-    if(req.session.oauth && req.session.oauth.access_token){
-        res.writeHeader(200, {"Content-Type": "text/html"});
-        res.end(index);
-    } else {
-        res.writeHead(302, {'Location': domain + '/twitter/auth'});
-        res.end();
-    }
-});
-
-
-/**
- * Twitter 3 Legged OAuth
- */
-
-/**
- * Twitter auth route
- */
-router.get('/twitter/auth', function(req, res){
-    req.session.oa = twitter.connect({
-        keys: {
-            consumer: 'GnB0DXhhkonUwDeG9Ardkw',
-            consumer_secret: 'iwWsFZeg9ncdWl3KjDy1X9mnw1uioN6ZFUx8loK68'
-        },
-        callback: domain + '/twitter/callback'
-    });
-
-    req.session.oa.getOAuthRequestToken(function(err, oauth_token, oauth_token_secret, results){
-        if (err) {
-            console.log(err);
-            res.send("Error: " + err);
-        } else {
-            req.session.oauth = {};
-            req.session.oauth.token = oauth_token;
-            req.session.oauth.token_secret = oauth_token_secret;
-            res.writeHead(302, {'Location': 'https://twitter.com/oauth/authenticate?oauth_token='+oauth_token});
-            res.end();
-        }
-    });
-});
-
-/**
- * Twitter Callback Route
- */
-router.get('/twitter/callback', function(req, res){
-
-    if (req.session.oauth) {
-        var url_parts = url.parse(req.url, true);
-        var query = url_parts.query;
-        console.log(query);
-        req.session.oauth.verifier = query.oauth_verifier;
-        var oauth = req.session.oauth;
-
-        req.session.oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier,
-            function(error, oauth_access_token, oauth_access_token_secret, results){
-                if (error){
-                    console.log(error);
-                } else {
-                    req.session.oauth.access_token = oauth_access_token;
-                    req.session.oauth,access_token_secret = oauth_access_token_secret;
-                    console.log(results);
-                    res.writeHead(302, {'Location': domain});
-                    res.end();
-                }
-            }
-        );
-    } else {
-        res.writeHead(302, {'Location': domain});
-        res.end();
-    }
-});
-
 
 /**
  * Server Listen Port 3000
@@ -108,72 +32,310 @@ server = http.createServer(router);
 server.listen(port);
 
 
-
-
 /**
- * WebSocket Response
+ * WebSocket Server
  */
 var wss = new WebSocketServer({port: 8080}),
-    trackers = []; // Contains currently open tweet trackers
+    connectionCount = 0,
+    tracker = null,
+    terms = [],
+    tweetCount = 0,
+    processedCount = 0,
+    cacheCount = 0,
+    batchCount = 0,
+    latestTweetId = null,
+    lastOldStreamTweetId = null,
+    firstNewStreamTweetId = null,
+    forcedEnd = false,
+    connectCount = 0;
 
 /**
  * New connection to Client
  */
 wss.on('connection', function (ws) {
 
-    console.log(ws.upgradeReq.headers);
+    // Keep track of clients
+    connectionCount++;
+    ws.id = "ws" + connectionCount;
 
     /**
      * Receive Message from Client
      */
     ws.on('message', function (message) {
-        message = message.trim();
+        message = message.trim().toLowerCase();
+        var trackMessage = false;
 
-        if (!trackers[message]) {
-            trackers[message] = {
-                tracker: null,
-                clients: [ws]
-            };
-
-            //TODO: GET SESSION FROM HTTP SERVER TO WS
-            trackers[message].tracker = twitter.stream({
-                keys: {
-                    consumer: 'GnB0DXhhkonUwDeG9Ardkw',
-                    consumer_secret: 'iwWsFZeg9ncdWl3KjDy1X9mnw1uioN6ZFUx8loK68',
-                    access_token: '488320252-o4VOIiikbvtVF7HyFD7Zb4rLJPHRD7JAre52symJ',
-                    access_token_secret: 'h95Hb2yqO0ciTIgmp7778MjojOPvVgHo7Ht6YD9nvWTAZ'
-                },
-                action: 'filter',
-                params: {
-                    track: message
-                }
-            });
-
-            trackers[message].tracker.on('tweet', function (tweet) {
-                trackers[message].clients.forEach(function(clientSocket){
-                    clientSocket.send(JSON.stringify(tweet));
-                });
-            });
-
-            trackers[message].tracker.on('delete', function () {
-                console.log('Must delete this tweet');
-            });
-
-            trackers[message].tracker.on('end', function () {
-                console.log('Stream ended');
-            });
+        if(message === "kill-twitter-connection"){
+            console.log("****************************************");
+            console.log("Twitter Connection Forcefully Destroyed!")
+            console.log("****************************************");
+            tracker.destroyConnection();
+            forcedEnd = true;
+            return;
+        } else if(message === "resume-twitter-connection"){
+            forcedEnd = false;
+        } else {
+            trackMessage = true;
         }
 
-        trackers[message].clients.push(ws);
+        if (!terms[message] || trackMessage === false) { // New term
+
+            if(trackMessage){
+                terms[message] = [];
+                terms[message]['tweets'] = [];
+                terms[message][ws.id] = ws;
+
+                // Destroy previous connection
+                if(tracker !== null) {
+                    console.log("*******************************************");
+                    console.log("Destroyed Previous Connection for New Terms")
+                    console.log("*******************************************");
+                    forcedEnd = true;
+                    tracker.destroyConnection();
+                }
+            }
+
+            startTracker();
+
+        } else {
+            // Term already being tracked add client to list
+            terms[message][ws.id] = ws;
+        }
     });
 
     /**
      * Connection to Client Closed
      */
-    ws.on('close', function () {
+    ws.on('close', function(){
+        console.log("Closed", ws.id);
+
+        // Remove client from terms
+        for(var key in terms){
+            var term = terms[key];
+
+            if (term[ws.id]) {
+                delete term[ws.id];
+                console.log("Removed client: ",  ws.id);
+            }
+
+            if (Object.keys(term).length == 1) {
+                delete terms[key];
+                console.log("Removed term: ",  key);
+                console.log("***************************************************");
+                console.log("Distributing tweets for" + Object.keys(terms).length + " terms, ", Object.keys(terms));
+                console.log("***************************************************");
+                // TODO: Remove term from data storage ?
+            }
+        }
+    });
+});
+
+
+function startTracker(){
+    // Create stream tracker
+    tracker = twitter.stream({
+        keys: twitterKeys,
+        action: 'filter',
+        params: {
+            track: Object.keys(terms).join(",")
+        }
+    });
+
+    /**
+     * Log New Connection
+     */
+    tracker.on("connection", function(){
+        connectCount += 1;
+        forcedEnd = false;
+        console.log("************************************************");
+        console.log("TWITTER CONNECTION MADE, CURRENT CONNECTIONS: ", connectCount)
+        console.log("************************************************");
+    });
+
+    /**
+     * Received a tweet
+     */
+    tracker.on('tweet', function(tweet){
+        if(lastOldStreamTweetId && !firstNewStreamTweetId){
+            // Recovery needed
+            distributeTweet(tweet, getMissedTweets);
+        } else {
+            distributeTweet(tweet);
+        }
+    });
+
+    /**
+     * Delete tweet notification
+     */
+    tracker.on('delete', function(){
+        console.log('Must delete this tweet');
+    });
+
+    /**
+     * Lost connection to twitter
+     */
+    tracker.on('end', function(){
+
+        connectCount -= 1;
+
+        console.log("************************************************");
+        console.log("TWITTER CONNECTION LOST, CURRENT CONNECTIONS: ", connectCount)
+        console.log("************************************************");
+
+        // Set the old stream tweet id for recovery
+        lastOldStreamTweetId = latestTweetId;
+        firstNewStreamTweetId = null;
+
+        if(!forcedEnd){
+            console.log('Stream ended, not done on purpose');
+
+            startTracker();
+
+        } else {
+            console.log('Stream forcefully ended');
+        }
 
     });
 
-});
+
+    /**
+     * Tracker error, possibly rate limited...
+     */
+    tracker.on("error", function(err){
+
+        //TODO: Reconnect after x time has elapsed
+
+        console.log(err);
+    })
+}
+
+/**
+ * Takes a tweet and distributes it to web socket clients
+ * @param tweet
+ */
+function distributeTweet(tweet, cb){
+
+    // This is the latest tweet id
+    latestTweetId = tweet.id_str;
+
+    // If this is new stream log first tweet id for recovery
+    if(!firstNewStreamTweetId){
+        firstNewStreamTweetId = latestTweetId;
+    }
+
+    // Get terms
+    var termKeys = Object.keys(terms);
+
+    // Loop through terms, check if its in the tweet and continue otherwise ignore
+    for(var i = 0; i < termKeys.length; i++) {
+        var term = termKeys[i];
+
+        if (tweet.text.toLowerCase().indexOf(term) > -1) {
+            // Check for duplicates for this term
+            if (!terms[term]['tweets'][tweet.id_str]) {
+
+                tweetCount++;
+                terms[term]['tweets'][tweet.id_str] = 1; // Mark tweet as received
+
+                // Batch Insert Begin
+                if(batchCount == 0){
+                    console.log("Begin Batch");
+                    tableService.beginBatch();
+                }
+
+                // Insert tweet
+                tableService.insertEntity(tableName, {
+                    PartitionKey: term,
+                    RowKey: tweet.id_str,
+                    tweet: tweet.text,
+                    created_at: tweet.created_at
+                });
+
+                batchCount++;
+
+                // Commit the batch
+                if(batchCount >= 50){
+                    var oldBatchCount = batchCount;
+                    batchCount = 0;
+
+                    console.log("Batching...", oldBatchCount)
+
+                    tableService.commitBatch(function(){
+                        console.log('Batched!');
+                        cacheCount += oldBatchCount;
+                    });
+                }
+
+                // Splice tweets if tooooo big
+                if (terms[term]['tweets'].length >= 2000) {
+                    terms[term]['tweets'].splice(0, 500);
+                }
+
+                // Message all clients with the term freq within tweet
+                var patt = new RegExp(term, "g");
+                var termCount = (tweet.text.toLowerCase().match(patt) || []).length;
+                var tweetLength = tweet.text.split(" ").length;
+
+                for (var clientKey in terms[term]) {
+                    if (clientKey !== 'tweets') {
+                        terms[term][clientKey].send(JSON.stringify({
+                            term: term,
+                            termCount: termCount,
+                            tweetLength: tweetLength
+                        }));
+                    }
+                }
+
+                processedCount++;
+                console.log("---------------------------------------------");
+                console.log("TWEETS: ", tweetCount, " | PROCESSED: ", processedCount, " | DB CACHED: ", cacheCount);
+                console.log("---------------------------------------------");
+            } else {
+                console.log("Duplicate Tweet", tweet.id_str);
+            }
+        }
+    }
+
+
+    if(typeof(cb) === 'function'){
+        cb();
+    }
+}
+
+
+/**
+ * Uses Twitter Search API to retrieve missing tweets for terms and distributes them
+ */
+function getMissedTweets(){
+    twitter.search({
+        keys: twitterKeys,
+        params: {
+            q: Object.keys(terms).join(" OR "),
+            since_id: lastOldStreamTweetId,
+            max_id: firstNewStreamTweetId,
+            count: 100,
+            result_type: 'recent'
+        }
+    }, function(err, data){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        var data = JSON.parse(data);
+
+        console.log("*******************************");
+        console.log("Recover Result Length", data.statuses.length)
+        console.log("*******************************");
+
+        for(var i = 0; i < data.statuses.length; i++){
+            var tweet = data.statuses[i];
+
+            console.log("Distributing Recovered Tweet", tweet.id_str);
+
+            distributeTweet(tweet);
+        }
+    });
+}
 
 
